cf22939b44a83f8402eec0f94cfdd638faa13371
Rory Conlon <roryconlon1@gmail.com>
2022-07-05 18:01:10 +0100

    First task

---
+++ exercise_a_stops.py
@@ -0,0 +1,45 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+#1. Add "Edinburgh Waverley" to the end of the list
+
+stops.append("Edinburgh Waverley")
+
+#2. Add "Glasgow Queen St" to the start of the list
+
+stops.insert(0, "Glasgow Queen St")
+
+#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+
+stops.insert(4, "Polmont")
+
+#4. Print out the index position of "Linlithgow"
+
+item = "Linlithgow"
+index = stops.index(item)
+
+#5. Remove "Livingston" from the list using its name
+
+stops.remove("Livingston")
+
+#6. Delete "Cumbernauld" from the list by index
+
+del stops[2]
+
+#7. Print the number of stops there are in the list
+
+num_items = len(stops)
+print(num_items)
+
+#8. Sort the list alphabetically
+
+stops = sorted(stops)
+
+#9. Reverse the positions of the stops in the list
+
+stops.reverse()
+
+#10 Print out all the stops using a for loop
+
+for stop in stops:
+    print(stop)
+
