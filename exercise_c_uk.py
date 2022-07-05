70b32eb9722f101456d633c859c1b9708231ee83
Rory Conlon <roryconlon1@gmail.com>
2022-07-05 20:26:50 +0100

    3rd task

--- exercise_c_uk.py
+++ exercise_c_uk.py
@@ -1,3 +1,11 @@
+#from tkinter.font import names
+
+
+from ast import Num
+import numbers
+from optparse import Values
+
+
 united_kingdom = [
   {
     "name": "Scotland",
@@ -17,6 +25,32 @@ united_kingdom = [
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+
+united_kingdom[1][2] = "Cardiff"
+
 # 2. Create a dictionary for Northern Ireland and add it to the `united_kingdom` list (The capital is Belfast, and the population is 1,811,000).
+
+Northern_Ireland = {
+  "name" : "Northern Ireland",
+  "population" : 1811000,
+  "capital" : "Belfast"
+  } 
+dictionary_copy = Northern_Ireland.copy()
+united_kingdom.append(dictionary_copy)
+
 # 3. Use a loop to print the names of all the countries in the UK.
+
+for country in united_kingdom:
+  print(country["name"])
+
 # 4. Use a loop to find the total population of the UK.
+
+
+#  #counter = 0 
+
+for amount in united_kingdom:
+    amount["population"]
+
+
+
+print(amount["population"])
